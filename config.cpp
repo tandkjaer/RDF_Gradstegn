63a9f9edefd78e84881f4cb8ca7ceb9a95bb0953
tandkjaer <thomas@andkjaer.dk>
2017-09-19 16:57:07 +0200

    config.cpp added

---
+++ config.cpp
@@ -0,0 +1,132 @@
+class CfgPatches 
+{ 
+    class RDF_Gradstegn 
+    { 
+        units[] = {"RDF_Gradstegn_Mod";}; 
+        weapons[] = {}; 
+        requiredVersion = 0.2; 
+        requiredAddons[] = {"A3_Characters_F"}; 
+    }; 
+}; 
+
+//************************************************************************************************************************************************************************************************
+//*****                          *********************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+
+class CfgUnitInsignia
+
+{
+    class RDF_PJ_Insignia
+    {
+		_generalMacro = "RDF_PJ_Insignia";
+        displayName = "Premierløjtnant"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom1_patch_co.paa"; // Image path
+        textureVehicle = "\RDF_Gradstegn\UI\custom1_patch_co.paa"; // Does nothing currently, reserved for future use
+	}
+	class RDF_SG_Insignia
+	{
+		_generalMacro = "RDF_SG_Insignia";
+		displayName = "Sergent"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom2_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_KS_Insignia
+	{
+		_generalMacro = "RDF_KS_Insignia";
+		displayName = "Konstabel"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom3_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_OKS_Insignia
+	{
+		_generalMacro = "RDF_OKS_Insignia";
+		displayName = "Overkonstabel"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom4_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_OKS1_Insignia
+	{
+		_generalMacro = "RDF_OKS1_Insignia";
+		displayName = "Overkonstabel af 1. grad"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom5_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_KP_Insignia
+	{
+		_generalMacro = "RDF_KP_Insignia";
+		displayName = "Korporal"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom6_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_OSG_Insignia
+	{
+		_generalMacro = "RDF_OSG_Insignia";
+		displayName = "Oversergent"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom7_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_SJ_Insignia
+	{
+		_generalMacro = "RDF_SJ_Insignia";
+		displayName = "Sekondløjtnant"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom8_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_KC_Insignia
+	{
+		_generalMacro = "RDF_KC_Insignia";
+		displayName = "Kaptajn"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom9_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_M_Insignia
+	{
+		_generalMacro = "RDF_M_Insignia";
+		displayName = "menig"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom0_patch_co.paa"; // Image path
+        textureVehicle = ""; // Does nothing currently, reserved for future use
+    };
+		class RDF_FL_Insignia
+	{
+		_generalMacro = "RDF_FL_Insignia";
+		displayName = "Flyvener_Løjtnant"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom10_patch_co.paa"; // Image path
+    };
+		class RDF_FK_Insignia
+	{
+		_generalMacro = "RDF_FK_Insignia";
+		displayName = "Flyvener_Kaptajn"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom11_patch_co.paa"; // Image path
+    };
+		class RDF_FE_Insignia
+	{
+		_generalMacro = "RDF_FE_Insignia";
+		displayName = "Flyvener_Elev"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom12_patch_co.paa"; // Image path
+    };
+		class RDF_FSG_Insignia
+	{
+		_generalMacro = "RDF_FE_Insignia";
+		displayName = "Flyvener_Sergent"; // Name displayed in Arsenal
+        author = "P. Petersen"; // Author displayed in Arsenal
+        texture = "\RDF_Gradstegn\UI\custom13_patch_co.paa"; // Image path
+    };
+	
+	
+}
+
+
+ 
